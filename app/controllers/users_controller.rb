cd5df870ad2f2f9149c901f2dfd4f8c436fd38aa
AlexandreMrl <mieral.alexandre@gmail.com>
2024-03-05 11:58:34 +0100

    --wip-- [skip ci]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = policy_scope(User)
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def new
+    @user = User.new
+    authorize @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    authorize @user
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    authorize @user
+    @user.destroy
+    redirect_to users_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root to: "pages#home"
-
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: 'pages#home'
+  resources :users
+  resources :auditions
+  resources :tags
+  resources :user_tags, only: [:create, :destroy]
+  resources :audition_tags, only: [:create, :destroy]
 end
