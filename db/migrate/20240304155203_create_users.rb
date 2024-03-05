4377d89d8326d4fd34103d8df0eb5a31b4aae492
lynda merzoud <lyndamerzoud@gmail.com>
2024-03-05 11:28:37 +0100

    add devise

--- db/migrate/20240304155203_create_users.rb
+++ db/migrate/20240304155203_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :password
       t.integer :phone_number
       t.string :social_link
