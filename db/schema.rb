34cf79aba6cf2199883895ff6471f6224117995a
Valentine Dague <daguevalentine@gmail.com>
2024-03-05 15:56:28 +0100

    ajout migration audition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_135748) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_145258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_135748) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "audition_date"
     t.index ["user_id"], name: "index_auditions_on_user_id"
   end
 
