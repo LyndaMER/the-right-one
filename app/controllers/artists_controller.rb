434234b5ed2aa8268cd134a8436f1b6c793989c1
AlexandreMrl <mieral.alexandre@gmail.com>
2024-03-06 11:18:20 +0100

    --wip-- [skip ci]

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,11 @@ class ArtistsController < ApplicationController
     @audition = Audition.find(params[:audition_id])
     @artists = ArtistsMatcherService.new(@audition).call
     @artists = policy_scope(Artist)
+    autorize @artist
   end
 
-
+  def show
+    @artist = Artist.find(params[:id])
+    authorize @artist
+  end
 end
