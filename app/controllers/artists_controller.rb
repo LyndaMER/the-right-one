b70712ea96584997a46b9a5c0ed4ffd844a7c8f7
lynda merzoud <lyndamerzoud@gmail.com>
2024-03-12 11:41:09 +0100

    add artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,49 @@
 class ArtistsController < ApplicationController
   skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
 
   def index
     @audition = Audition.find(params[:audition_id])
     @artists = ArtistsMatcherService.new(@audition).call.uniq
-    # @tags = @artists.flat_map(&:tags).uniq
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(artist_params)
+    if @user.save!
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @artist = User.find(params[:id])
+  end
+
+  def edit
+    @artist = User.find(params[:id])
+  end
+
+  def update
+    @artist = User.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def destroy
+    @artist = User.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+
+  def artist_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :address, :phone_number, :birth_date, :pola_face, :portfolio, :social_link)
+  end
+
 end
